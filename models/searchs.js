2dac4bde4ab19aa7fc42397dd6f52499c313d227
Facundo Aquino <facun90@live.com>
2021-03-05 18:27:23 -0300

    app finished , adding history whit .json

--- models/searchs.js
+++ models/searchs.js
@@ -1,34 +1,36 @@
 const axios = require('axios')
 require('dotenv').config()
-
+const fs = require('fs')
 
 class Searchs {
-	constructor() {}
+	constructor() {
+		this.readDb()
+	}
 
-    get paramasMapbox(){
-        return {
-            'access_token':process.env.MAPBOX_KEY,
-                    'limit':5,
-                    'language':'es'
-        }
-    }
+	history = []
+	pathDb = './db/history.json'
+	get paramasMapbox() {
+		return {
+			access_token: process.env.MAPBOX_KEY,
+			limit: 5,
+			language: 'es',
+		}
+	}
 
 	async city(place = '') {
 		// console.log(place);
 		try {
-
-            const instance = axios.create({
-                baseURL:`https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json`,
-                params:this.paramasMapbox
-            })
-
+			const instance = axios.create({
+				baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json`,
+				params: this.paramasMapbox,
+			})
 
 			const response = await instance.get()
-			const cities = response.data.features.map(city=>({
-				id:city.id,
+			const cities = response.data.features.map((city) => ({
+				id: city.id,
 				name: city.place_name,
 				lng: city.center[0],
-				lat: city.center[1]
+				lat: city.center[1],
 			}))
 
 			return cities
@@ -36,6 +38,51 @@ class Searchs {
 			console.log(err)
 			return []
 		}
+	}
+
+	get paramsOpenWeather() {
+		return {
+			appid: process.env.OPENWEATHER_KEY,
+			units: 'metric',
+			lang: 'es',
+		}
+	}
+
+	async weatherPlace(lat, lon) {
+		const instance = axios.create({
+			baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+			params: { ...this.paramsOpenWeather, lat, lon },
+		})
+		const response = await instance.get()
+
+		const { weather, main } = await response.data
+		// console.log(weather[0].description, main)
+
+		return {
+			desc: weather[0].description,
+			min: main.temp_min,
+			max: main.temp_max,
+			temp: main.temp,
+		}
+	}
+
+	pushHistory(place = '') {
+		if (!this.history.includes(place)) {
+			this.history.unshift(place)
+			
+		}
+
+		this.saveDb()
+	}
+
+	saveDb() {
+		fs.writeFileSync(this.pathDb, JSON.stringify(this.history, null, 5))
+	}
+
+	readDb() {
+		const readHistory = JSON.parse(fs.readFileSync(this.pathDb, { encoding: 'utf-8' }))
+
+		this.history = readHistory
 	}
 }
 
